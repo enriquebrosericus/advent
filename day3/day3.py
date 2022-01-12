759bf3ddeab79ac9cb753fffb6fa54e04b3727c8
eb <ericbrose@gmail.com>
2022-01-11 22:55:56 -0800

    working

--- day3/day3.py
+++ day3/day3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-with open('input.txt', 'r') as infile:
+with open('/Users/edawg/gitsome/advent/day3/input.txt', 'r') as infile:
     data = [line.strip() for line in infile]
 
 
@@ -11,6 +11,42 @@ biglist=[]
 for i in data:
     #list comprehension 
     x = [int(a) for a in str(i)]
-    print(x)
-    #print(i)
-    #biglist.append(x)
+    #print(x)
+    biglist.append(x)
+
+#find most common bit value in each position and store that in a list
+
+#scrape length of element - could just hard code since it's 12
+itemlength=len(biglist[0])
+biglistlength=len(biglist)
+
+i=0
+j=0
+
+#1st element is 0 totals for each position
+#2nd element is 1 totals for each position
+gamma_totals = [[0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0]]
+                
+print(len(biglist))
+
+while i < biglistlength:
+    while j < itemlength:
+        print(biglist[i][j]
+        )
+        if biglist[i][j] == 0:
+            print ("It's a zero")
+            gamma_totals[0][j] += 1
+            j +=1
+        else :
+            gamma_totals[1][j] += 1
+            j +=1
+    i +=1
+
+
+print(gamma_totals)
+
+
+
+
+
+
