aeceef2255676568899656599aa4f12e429fe59e
Eric Brose <eb64299@yahoo.com>
2022-01-12 05:58:11 +0000

    day3

---
+++ day3/day3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+with open('input.txt', 'r') as infile:
+    data = [line.strip() for line in infile]
+
+
+#need to put this in a multidimesional list
+
+biglist=[]
+
+    for i in data:
+        #list comprehension 
+        x = [int(a) for a in str(i)]
+        print(x)
+        #print(i)
+        #biglist.append(x)
